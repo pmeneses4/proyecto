d2f4d4e2074a9e36496fc38283b04638b23592f6
pmeneses4 <pollo_desu@comunidad.unam.mx>
2018-08-31 12:09:15 -0500

    Cambios en los parametros

--- proyecto.sh
+++ proyecto.sh
@@ -2,7 +2,7 @@
 
 echo  "Esto es una prueba"
 
-echo $(dmesg | tail -n 12 | grep "Serial" | awk '{print $6}')
+echo $(dmesg | tail -n 12 | grep "Serial" | awk '{print $6}')  #Antes estaba con el parametro de tail - 20 y print $ 5; aunque no quedaba como se deseaba
 
 #sudo mount /dev/sdc1 /mnt #Monta el usb. Se eligió sdb1, porque fue el que se detecto
 
