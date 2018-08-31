4cda9cf73127d1c902e0301e90217ed7a799f8a7
pmeneses4 <pollo_desu@comunidad.unam.mx>
2018-08-31 11:58:52 -0500

    Actualizacion de pruebas con n -12 (Renglon con el serial de la usb)

--- proyecto.sh
+++ proyecto.sh
@@ -2,4 +2,8 @@
 
 echo  "Esto es una prueba"
 
-echo $(dmesg)
+echo $(dmesg | tail -n 12 | grep "Serial" | awk '{print $6}')
+
+#sudo mount /dev/sdc1 /mnt #Monta el usb. Se eligió sdb1, porque fue el que se detecto
+
+#Prueba
